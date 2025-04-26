9274bf6454d9c2c3f8ce65c3889935fd1a78df0e
sfcg2 <castillo.sofia@correounivalle.edu.co>
2025-04-26 16:04:56 -0500

    Cambios login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,56 +1,58 @@
 import { Link } from "react-router-dom";
+import imagen1 from "../images/imagensign-in.jpg"
+import imagen2 from "../images/logo.png"
 
 export default function Login() {
   return (
-    <div className="flex min-h-screen">
+    <div className="flex min-h-screen rounded-xl">
       {/* Imagen a la izquierda */}
-      <div className="hidden md:flex md:w-2/5 items-center justify-center p-4">
+      <div className="hidden md:flex md:w-2/5 items-center justify-center p-0 rounded-xl">
         <img
-          src="/imagen1.png" 
+          src={imagen1} 
           alt="Escena del crimen"
-          className="w-4/5 h-auto object-cover rounded-lg shadow-md"
+          className="w-full h-full object-cover rounded-xl shadow-lg"
         />
       </div>
       
       {/* Formulario a la derecha */}
       <div className="flex flex-col items-center justify-center w-full md:w-3/5 p-8 bg-white shadow-lg">
-        <div className="flex flex-col items-center">
-          <span className="text-red-500 text-4xl">📍</span>
-          <h2 className="text-2xl font-bold mt-2">Sign in to GeoCrimen</h2>
-        </div>
-
-        <form className="mt-6 w-full max-w-sm"> //Usuario 
-          <div>
-            <label className="block text-sm font-medium">Username</label>
-            <input
-              type="text"
-              className="w-full p-2 border rounded mt-1 focus:outline-none focus:ring-2 focus:ring-blue-400"
-            />
+          <div className="flex flex-col items-center mt-10">
+            <img src={imagen2} alt="Logo" className="w-20" />            
+            <h2 className="text-4xl font-bold mt-2">Sign in to GeoCrimen</h2>
           </div>
 
-          <div className="mt-4">//PASSWORD
-            <label className="block text-sm font-medium">Password</label>
-            <input
-              type="password"
-              className="w-full p-2 border rounded mt-1 focus:outline-none focus:ring-2 focus:ring-blue-400"
-            />
-          </div>
-          <Link to="/forgotP1">
-            <p className="grid justify-items-end mt-4 text-sm underline underline-offset-1">
-              Forgot Password?</p>
-          </Link>
+          <form className="mt-10 w-full max-w-sm space-y-4">
+            <div>
+              <label className="mt-5 block text-sm font-medium">Username</label>
+              <input
+                type="text"
+                className="w-full p-2 border border-black/50 rounded mt-1"
+              />
+            </div>
 
-          <button
-            type="submit"
-            className="w-full mt-6 bg-blue-600 text-white py-2 rounded hover:bg-blue-700 transition"
-          >
-            Sign in
-          </button>
-        </form>
+            <div className="mt-20">
+              <label className="block text-sm font-medium">Password</label>
+              <input
+                type="password"
+                className="w-full p-2 border border-black/50 rounded mt-1"
+              />
+            </div>
+            <Link to="/forgotP1">
+              <p className="grid justify-items-end mt-4 text-sm underline underline-offset-1">
+                Forgot Password?</p>
+            </Link>
 
-        <p className="mt-4 text-sm">
-          Don't have an account? <Link to="/register" className="text-blue-600">Sign up</Link>
-        </p>
+            <button
+              type="submit"
+              className="w-20 mt-20 mx-40 bg-white border border-black text-black py-2 rounded transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-black hover:text-white"
+            >
+              Sign in
+            </button>
+          </form>
+
+          <p className="mt-4 text-sm">
+            Don't have an account? <Link to="/register" className="text-blue-600">Sign up</Link>
+          </p>
       </div>
     </div>
   );
