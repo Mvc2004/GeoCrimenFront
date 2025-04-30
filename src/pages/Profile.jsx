bf5e90881771c4e65642705f12310b7cc5ab828b
sfcg2 <castillo.sofia@correounivalle.edu.co>
2025-04-30 13:33:42 -0500

    Cambios Profile falta cambiar colores

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import imagen1 from "../images/usuario.png";
+import { PencilSquareIcon, UserCircleIcon} from '@heroicons/react/24/solid'
 
 function Profile() {
+  
   const [isEditable, setIsEditable] = useState({
     username: false,
     email: false,
@@ -27,7 +29,7 @@ function Profile() {
     <div className="min-64-screen bg-[#E0E0E0]">
       <div className="flex flex-col md:flex-row min-h-screen">
         {/* Partición izquierda */}
-        <div className="flex flex-col w-full md:w-1/5 p-8 bg-[#E0E0E0]">
+        <div className="flex flex-col w-full md:w-1/5 p-8 bg-s">
           <div className="grid justify-items-center bg-white"></div>
         </div>
 
@@ -35,121 +37,85 @@ function Profile() {
         <div className="w-full md:w-3/5 p-8 bg-[#E0E0E0]">
           <div className="flex flex-col items-center justify-center h-screen">
             <div className="flex flex-col items-center">
-              <span className="mt-10 text-red-500 text-4xl">
-                <img src={imagen1} alt="Logo" className="w-40" />
+              <span className="mt-10 text-black text-4xl">
+                <UserCircleIcon className="w-40" />
               </span>
             </div>
 
-            {/* Formulario */}
-            <form className="mt-20 w-full max-w-sm">
-              {/* Campo Username */}
-              <div className="relative mt-5">
-                <label className="block text-sm font-medium">Username</label>
-                <input
-                  type="text"
-                  name="username"
-                  value={formData.username}
-                  readOnly={!isEditable.username}
-                  onChange={handleChange}
-                  className="w-full p-2 pr-10 border border-black/50 rounded mt-1 focus:outline-none focus:ring-2 focus:ring-blue-400"
-                />
-                <div
-                  className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer"
-                  onClick={() => handleEditClick("username")}
-                >
-                  <svg
-                    className="w-5 h-5 text-gray-500 hover:text-blue-500"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M11 4H4v16h16v-7m-3-3l4-4a2.828 2.828 0 00-4-4l-4 4m0 0L7 17l4 4 9-9"
-                    ></path>
-                  </svg>
-                </div>
-              </div>
+            <div className="relative flex flex-col rounded-xl bg-transparent">
+  <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+    <div className="mb-1 flex flex-col gap-6">
+      {/* Username */}
+      <div className="w-full max-w-sm min-w-[200px] relative">
+        <label className="block mb-2 text-sm text-slate-600">Username</label>
+        <input
+          type="text"
+          name="username"
+          value={formData.username}
+          readOnly={!isEditable.username}
+          onChange={handleChange}
+          placeholder="Username"
+          className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 pr-10 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+        />
+        <div
+          className="absolute right-2 top-1/2 -translate-y-1/2 cursor-pointer"
+          onClick={() => handleEditClick("username")}
+        >
+          <PencilSquareIcon className="h-6 w-6 text-gray-500 mt-5" />
+        </div>
+      </div>
 
-              {/* Campo Email */}
-              <div className="relative mt-5">
-                <label className="block text-sm font-medium">Email</label>
-                <input
-                  type="email"
-                  name="email"
-                  value={formData.email}
-                  readOnly={!isEditable.email}
-                  onChange={handleChange}
-                  className="w-full p-2 pr-10 border border-black/50 rounded mt-1 focus:outline-none focus:ring-2 focus:ring-blue-400"
-                />
-                <div
-                  className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer"
-                  onClick={() => handleEditClick("email")}
-                >
-                  <svg
-                    className="w-5 h-5 text-gray-500 hover:text-blue-500"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M11 4H4v16h16v-7m-3-3l4-4a2.828 2.828 0 00-4-4l-4 4m0 0L7 17l4 4 9-9"
-                    ></path>
-                  </svg>
-                </div>
-              </div>
+      {/* Email */}
+      <div className="w-full max-w-sm min-w-[200px] relative">
+        <label className="block mb-2 text-sm text-slate-600">Email</label>
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          readOnly={!isEditable.email}
+          onChange={handleChange}
+          placeholder="Your Email"
+          className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 pr-10 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+        />
+        <div
+          className="absolute right-2 top-1/2 -translate-y-1/2 cursor-pointer"
+          onClick={() => handleEditClick("email")}
+        >
+          <PencilSquareIcon className="h-6 w-6 text-gray-500 mt-5" />
+        </div>
+      </div>
 
-              {/* Campo Password */}
-              <div className="relative mt-5">
-                <label className="block text-sm font-medium">Password</label>
-                <input
-                  type="password"
-                  name="password"
-                  value={formData.password}
-                  readOnly={!isEditable.password}
-                  onChange={handleChange}
-                  className="w-full p-2 pr-10 border border-black/50 rounded mt-0.1 focus:outline-none focus:ring-2 focus:ring-blue-400"
-                />
-                <div
-                  className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer"
-                  onClick={() => handleEditClick("password")}
-                >
-                  <svg
-                    className="w-5 h-5 text-gray-500 hover:text-blue-500"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M11 4H4v16h16v-7m-3-3l4-4a2.828 2.828 0 00-4-4l-4 4m0 0L7 17l4 4 9-9"
-                    ></path>
-                  </svg>
-                </div>
-              </div>
+      {/* Password */}
+      <div className="w-full max-w-sm min-w-[200px] relative">
+        <label className="block mb-2 text-sm text-slate-600">Password</label>
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          readOnly={!isEditable.password}
+          onChange={handleChange}
+          placeholder="Your Password"
+          className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 pr-10 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+        />
+        <div
+          className="absolute right-2 top-1/2 -translate-y-1/2 cursor-pointer"
+          onClick={() => handleEditClick("password")}
+        >
+          <PencilSquareIcon className="h-6 w-6 text-gray-500 mt-5" />
+        </div>
+      </div>
+    </div>
 
-              {/* Botón Update */}
-              <Link to="/">
-                <div className="flex flex-col items-center justify-center">
-                  <button
-                    type="button"
-                    className="w-60 mt-20 bg-[#FFFFFF] text-[#2E8B57] py-2 rounded border border-[#2E8B57] hover:bg-[#2E8B57] hover:text-[#FFFFFF]"
-                  >
-                    Update
-                  </button>
-                </div>
-              </Link>
-            </form>
+    {/* Botón */}
+    <button
+      type="button"
+      className="mt-4 w-full rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+    >
+      Actualizar
+    </button>
+  </form>
+</div>
+
           </div>
         </div>
 
