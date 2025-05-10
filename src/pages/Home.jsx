0591888a7a229b244c2f18078daccaf7e9a3021e
sfcg2 <castillo.sofia@correounivalle.edu.co>
2025-05-10 03:03:25 -0500

    nuevo home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
+import {UserGroupIcon } from "@heroicons/react/24/solid";
+
 import im1 from '../images/im1.jpeg'
 import im2 from '../images/im2.jpeg'
 import im3 from '../images/im3.jpeg'
@@ -7,9 +9,26 @@ import im4 from '../images/im4.jpeg'
 import im5 from '../images/im5.jpeg'
 import im6 from '../images/im6.jpeg'
 import im7 from '../images/im7.jpeg'
+import color1 from '../images/00ffff.png';
+import color2 from '../images/003fff.png';
+import color3 from '../images/bf001f.png';
 import logo from '../images/logo.png';
 
+import { useState } from "react";
+
 function Home() {
+
+  const [activeIndex, setActiveIndex] = useState(0);
+  const images = [im1, im2, im3, im4, im5, im6, im7];
+
+  const handlePrev = () => {
+    setActiveIndex((prev) => (prev - 1 + images.length) % images.length);
+  };
+
+  const handleNext = () => {
+    setActiveIndex((prev) => (prev + 1) % images.length);
+  };
+
   {/*const [map, setMap] = useState(null);
   const [heatmap, setHeatmap] = useState(null);
 
@@ -106,93 +125,117 @@ function Home() {
         </div>
       </header>
 
-      {/* Mapa interactivo 
-      <div className="flex justify-center my-10">
-        <div 
-          id="map" 
-          className="w-4/5 h-100 rounded-md shadow-lg" 
-          style={{ height: "500px" }}
-        />
-      </div>
 
-      {/* Controles del mapa 
-      <div className="flex justify-center space-x-4 mb-8 mt-20">
-        <button 
-          className="px-4 py-2 text-white font-bold rounded-md border-2 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-white/50 hover:text-black"
-          onClick={() => heatmap && heatmap.setMap(heatmap.getMap() ? null : map)}
-        >
-          Mostrar/Ocultar Mapa de Calor
-        </button>
-      </div>*/}
+        <div className="relative mt-10 h-[500px] ml-[130px] w-[1250px] overflow-hidden flex justify-center items-center bg-black">
+          {images.map((img, i) => (
+          <img
+            key={i}
+            src={img}
+            className={`absolute h-[500px] w-full max-w-full ${
+            i === activeIndex ? "opacity-100 z-10" : "opacity-0 z-0"
+            }`}
+            alt={`Slide ${i + 1}`}
+          />
+          ))}
+        </div>
 
-      {/* Información de denuncias */}
-      <div className="mt-20 flex flex-col m-auto p-auto">
-        <div className="flex overflow-x-scroll pb-10 hide-scroll-bar">
-          <div className="flex flex-nowrap min-w-[150%] lg:ml-1 md:ml-20 ml-10">
-            <div className="inline-block px-6">
-              <div className="mt-10 w-lg max-w-xs overflow-hidden transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 rounded-xl hover:shadow-xl">
-                <img src={im1} alt="Imagen 1 Carrusel" className="w-80 h-64 rounded-md shadow-md"/>
-              </div>
+        <div className="absolute top-[340px] left-[40px] z-30 flex items-center justify-center">
+          <button
+            onClick={handlePrev}
+            className="flex items-center justify-center h-[50px] w-[50px] bg-[#003049] rounded-full group"
+          >
+            <ChevronLeftIcon className="w-[30px] h-[30px] text-white" strokeWidth={4.5} />
+          </button>
+        </div>
+        <div className="absolute top-[340px] right-[40px] z-30 flex items-center justify-center">
+          <button
+            onClick={handleNext}
+            className="flex items-center justify-center h-[50px] w-[50px] bg-[#003049] rounded-full group"
+          >
+            <ChevronRightIcon className="w-[30px] h-[30px] text-white" strokeWidth={4.5} />
+          </button>
+        </div>
+            
+        <div className="flex flex-row items-start justify-between mt-20 h-[400px] w-full px-10">
+          <div className="text-left bg-white ml-[92px] w-[800px]">
+            <h3 className="font-bold text-3xl mb-2 mt-20" style={{ fontFamily: 'Arial Black, sans-serif' }}>
+              <span className="text-black">Sobre</span>{' '}
+              <span className="text-[#D62828]">Nosotros</span>
+            </h3>
+            <h4 className="text-black text-2xl text-justify mt-10" style={{ fontFamily: 'Arial, sans-serif' }}>
+              Geocrimen es una plataforma que permite a los ciudadanos reportar y visualizar delitos en cualquier parte de Colombia. 
+              Utilizando tecnología de mapas de calor, los usuarios podrán observar la ubicación y naturaleza de los delitos, 
+              lo que ayuda a crear conciencia y fomentar la seguridad comunitaria. 
+            </h4>
+          </div>
+          <div className="flex items-center justify-center mt-10 ml-[200px] w-[1000px] h-full">
+            <UserGroupIcon className="w-[400px] h-[400px] text-[#003049]" strokeWidth={4.5} />
+          </div>
+        </div>
+        <div className="w-full py-12 px-4 mt-[150px]" style={{ backgroundColor: "#003049" }}>
+        <div class="mt-10 flex justify-center items-center gap-40 flex-wrap">
+          {/*-- Tarjeta 1 -->*/}
+          <div class="max-w-xs bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700"  style={{ backgroundColor: '#FFFFFF' }}>
+            <a href="#">
+            <img class="rounded-t-2xl" src={color3} alt="color1" style={{border: ' 20px solid #FFFFFF',borderRadius: '1.7rem' }}></img>
+            </a>
+            <div class="p-5">
+              <a href="#">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-[#003049]">
+                  #bf001f
+                </h5>
+              </a>
+              <p class="mb-3 font-normal text-gray-700 text-[#003049]">
+                Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+              </p>
+              
             </div>
-            <div className="inline-block px-6">
-              <div className="mt-10 w-lg max-w-xs overflow-hidden transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 rounded-xl hover:shadow-xl">
-                <img src={im2} alt="Imagen 2 Carrusel" className="w-80 h-64 rounded-md shadow-md"/>
-              </div>
+          </div>
+          {/*-- Tarjeta 2 -->*/}
+          <div class="max-w-xs bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700"  style={{ backgroundColor: '#FFFFFF' }}>
+            <a href="#">
+            <img class="rounded-t-2xl" src={color2} alt="color1" style={{border: ' 20px solid #FFFFFF',borderRadius: '1.7rem' }}></img>
+            </a>
+            <div class="p-5">
+              <a href="#">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-[#003049]">
+                  #003fff
+                </h5>
+              </a>
+              <p class="mb-3 font-normal text-gray-700 text-[#003049]">
+                Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+              </p>
+              
             </div>
-            <div className="inline-block px-6">
-              <div className="mt-10 w-lg max-w-xs overflow-hidden transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 rounded-xl hover:shadow-xl">
-                <img src={im3} alt="Imagen 3 Carrusel" className="w-80 h-64 rounded-md shadow-md"/>
-              </div>
-            </div>
-            <div className="inline-block px-6">
-              <div className="mt-10 w-lg max-w-xs overflow-hidden transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 rounded-xl hover:shadow-xl">
-                <img src={im4} alt="Imagen 4 Carrusel" className="w-80 h-64 rounded-md shadow-md"/>
-              </div>
-            </div>
-            <div className="inline-block px-6">
-              <div className="mt-10 w-lg max-w-xs overflow-hidden transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 rounded-xl hover:shadow-xl">
-                <img src={im5} alt="Imagen 5 Carrusel" className="w-80 h-64 rounded-md shadow-md"/>
-              </div>
-            </div>
-            <div className="inline-block px-6">
-              <div className="mt-10 w-lg max-w-xs overflow-hidden transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 rounded-xl hover:shadow-xl">
-                <img src={im6} alt="Imagen 6 Carrusel" className="w-80 h-64 rounded-md shadow-md"/>
-              </div>
-            </div>
-            <div className="inline-block px-6">
-              <div className="mt-10 w-lg max-w-xs overflow-hidden transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 rounded-xl hover:shadow-xl">
-                <img src={im7} alt="Imagen 7 Carrusel" className="w-80 h-64 rounded-md shadow-md"/>
-              </div>
+          </div>
+          {/*-- Tarjeta 3 -->*/}
+          <div class="max-w-xs bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700"  style={{ backgroundColor: '#FFFFFF' }}>
+            <a href="#">
+            <img class="rounded-t-2xl" src={color1} alt="color1" style={{border: ' 20px solid #FFFFFF',borderRadius: '1.7rem' }}></img>
+            </a>
+            <div class="p-5">
+              <a href="#">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-[#003049]">
+                #00ffff
+                </h5>
+              </a>
+              <p class="mb-3 font-normal text-gray-700 text-[#003049]">
+                Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+              </p>
+              
             </div>
           </div>
         </div>
-      </div>
-      <style>
-        {`
-          .hide-scroll-bar {
-            -ms-overflow-style: none;
-            scrollbar-width: none;
-          }
-          .hide-scroll-bar::-webkit-scrollbar {
-            display: none;
-          }
-        `}
-      </style>
       
-      <div className="grid grid-rows-2 flex justify-center space-x-20 mt-40 mb-4">
-        <h3 class="text-balance text-white font-bold text-4xl">!LOCALIZA EL CRIMEN</h3>
-        <h4 class="text-balance text-white font-bold text-4xl">Y REPORTALO!</h4>
+        <div className="flex justify-center space-x-20 mt-[100px] ">
+          <img src={logo} alt="Logo" className="w-[50px]" />
+        </div>
 
-      </div>
-
-      <div className="flex justify-center space-x-20 mt-40 mb-4">
-        <img src={logo} alt="Logo" className="w-11" />
-      </div>
-
-      {/* Footer */}
-      <footer className="text-center mt-4 pb-4 text-gray-500 text-sm">
+              {/* Footer */}
+      <footer className="text-center mt-4 pb-4 text-white text-md">
         <p>© 2025 GeoCrimen. All rights reserved.</p>
       </footer>
+      </div>
     </div>
   );
 }
